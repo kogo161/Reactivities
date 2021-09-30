f5c0e48f49191a74da0333e804fa86337352ec74
Sasha Jgharkava <68333112+sasha596000033@users.noreply.github.com>
2021-09-30 17:48:21 +0400

    first commit

---
+++ API/Controllers/BaseApiController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BaseApiController : ControllerBase
+    {
+        public BaseApiController()
+        {
+
+        }
+    }
+}
