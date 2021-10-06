072e6cd17ca5dbd37588ef277bf7d3a7c9f9f490
kogo161 <91289023+kogo161@users.noreply.github.com>
2021-10-06 21:15:11 +0400

    add cqrs

--- API/Startup.cs
+++ API/Startup.cs
@@ -8,6 +8,8 @@ using Persistence;
 using Microsoft.EntityFrameworkCore;
 using Application.Activities;
 using MediatR;
+using AutoMapper;
+using Application.Core;
 
 namespace API
 {
@@ -45,6 +47,7 @@ namespace API
             });
 
             services.AddMediatR(typeof(List.Handler).Assembly);
+            services.AddAutoMapper(typeof(MappingProfiles).Assembly);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
